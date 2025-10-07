fcda394483dc3ed61a498262c2e981cc99d77699
Vikas Verma <vikas.verma@techplusinfotech.com>
2025-10-07 12:33:38 +0530

    updated vite.config.ts and vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // or your framework plugin
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // 👈 important for correct file paths on Vercel
+  build: {
+    outDir: "dist",
+  },
+});
